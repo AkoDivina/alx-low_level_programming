a4d67d0a55c6fabcee77245531dd691e829ae7e9
AkoDivina <divinako963@gmail.com>
2023-10-16 14:14:05 -0700

    .

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 // to swap a new varable is created which holds the value of a, pointer a is then empty and can hold the value in b, b is then empty and can now hold the value in a 
+*c = *b
 void swap_int(int *a, int *b)
 {
 	*c = *a;
